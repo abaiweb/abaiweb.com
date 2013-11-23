ee0bc98e2202f14c38db21988810ba7fb52f47b8
abaiweb <sky-xiaobai@qq.com>
2013-11-23 21:39:38 +0800

    Create test_20131123.php

---
+++ test_20131123.php
@@ -0,0 +1,16 @@
+<?php
+  class  Test{
+    
+    public function __construct(){
+      
+    }
+    
+    public function test(){
+      $this->display();
+    }
+    
+    public function test1(){
+      echo '你好我是测试的github';
+    }
+  }
+?>
